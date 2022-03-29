427d6afd7bc521b40c2af045963f6ad502114d13
atesi <96477657+fkelfk@users.noreply.github.com>
2022-03-29 10:35:30 +0900

    20220329

---
+++ lesson_2_1/2_1.py
@@ -0,0 +1,52 @@
+n = 12
+m = n + 1
+num = 0
+k = 3
+list = []
+
+'''
+while num <= n:
+    num = num + 1
+    b = len(list)
+    if n % num == 0:
+        list.append(num)
+    print(list[k - 1])
+'''
+
+
+while num < n:
+    num = num + 1
+    if n % num == 0:
+        list.append(num)
+        if len(list) > k:
+            print(list[k-1])
+            break
+else:
+    print(-1)
+
+
+     
+
+'''
+def A():
+    while num < n:
+        num = num + 1
+        if n % num == 0:
+            list.append(num)
+#함수로 만들어 볼까?
+print(A)
+#실패
+'''
+#중첩반목문이용
+
+#for문
+
+for divisor in range(1,n+1):
+    if n % divisor == 0:
+        list.append(divisor)
+        print(list)
+        if len(list) > k:
+            print(list[k-1])
+            break
+else:
+    print(-1)
